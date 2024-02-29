fbe98d9bbe0273a7f85486b2728af44c115df4fd
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-02-29 07:08:05 +0530

    Time: 9 ms (99.22%) | Memory: 66.1 MB (50.98%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -20,20 +20,18 @@ class Solution {
         int ind = 0;
         while(!queue.isEmpty()){
             int size = queue.size();
-            int prev = 0;
-            if((ind & 1) == 0){
-                prev = Integer.MIN_VALUE;
-            }
-            else{
-                prev = Integer.MAX_VALUE;
-            }
+            int prev = (ind & 1) == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             while(size-- > 0){
                 TreeNode current = queue.poll();
-                if((ind & 1) == 0 && ((current.val & 1) == 0 || prev >= current.val)){
-                    return false;
+                if((ind & 1) == 0){
+                    if((current.val & 1) == 0 || prev >= current.val){
+                        return false;
+                    }
                 }
-                if((ind & 1) == 1 && ((current.val & 1) == 1 || prev <= current.val)){
-                    return false;
+                if((ind & 1) == 1){
+                    if((current.val & 1) == 1 || prev <= current.val){
+                        return false;
+                    }
                 }
                 prev = current.val;
                 if(current.left != null){
