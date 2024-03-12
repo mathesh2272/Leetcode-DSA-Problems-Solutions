164efc4b3853d140453c066f12d302a60c5433ed
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-03-12 10:14:12 +0530

    Time: 11 ms (30.63%) | Memory: 45.1 MB (18.10%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        Map<Integer, LinkedList<Integer>> map = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
         int n = mat.length;
         int m = mat[0].length;
         int cnt = 0;
         for(int i = 0 ; i < n ; i++){
             int val = cnt;
             for(int j = m - 1 ; j >= 0 ; j--){
-                map.putIfAbsent(val, new LinkedList<>());
+                map.putIfAbsent(val, new PriorityQueue<>());
                 map.get(val).add(mat[i][j]);
                 val++;
             }
             cnt++;
         }
-        for(int ele : map.keySet()){
-            LinkedList<Integer> list = new LinkedList<>(map.get(ele));
-            Collections.sort(list);
-            map.put(ele, list);
-        }
         cnt = 0;
         for(int i = 0 ; i < n ; i++){
             int val = cnt;
             for(int j = m - 1 ; j >= 0 ; j--){
-                mat[i][j] = map.get(val).pollFirst();
+                mat[i][j] = map.get(val).poll();
                 val++;
             }
             cnt++;
