f8ab8d4c1b9c38574ea20eb046d373e1311b94e3
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-04-24 16:30:01 +0530

    Time: 21 ms (77.11%) | Memory: 55.5 MB (74.42%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,29 +1,42 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) 
-            return Collections.singletonList(0);
-        List<Set<Integer>> adj = new ArrayList<>(n);
-        for(int i = 0 ; i < n ; i++)
-            adj.add(new HashSet<>());
-        for(int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        List<Integer> ans = new ArrayList<>();
+        if(n == 1){
+            ans.add(0);
+            return ans;
         }
-        List<Integer> leaves = new ArrayList<>();
-        for(int i = 0 ; i < n ; i++)
-            if(adj.get(i).size() == 1) 
-                leaves.add(i);
-        while(n > 2) {
-            n -= leaves.size();
-            List<Integer> newLeaves = new ArrayList<>();
-            for(int i : leaves) {
-                int j = adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if(adj.get(j).size() == 1)
-                    newLeaves.add(j);
+        List<Integer>[] graph = new ArrayList[n];
+        for(int i = 0 ; i < n ; i++){
+            graph[i] = new ArrayList<>();
+        }
+        int[] indegree = new int[n];
+        for(int[] edge : edges){
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+            indegree[edge[0]]++;
+            indegree[edge[1]]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 1){
+                queue.add(i);
             }
-            leaves = newLeaves;
         }
-        return leaves;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            ans = new ArrayList<>();
+            while(size-- > 0){
+                int ele = queue.poll();
+                ans.add(ele);
+                indegree[ele]--;
+                for(int val : graph[ele]){
+                    indegree[val]--;
+                    if(indegree[val] == 1){
+                        queue.add(val);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
