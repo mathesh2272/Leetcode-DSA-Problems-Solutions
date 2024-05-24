30ab71114af3b3b0867dd5dc516fc4dc7cf4e948
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-05-24 07:44:06 +0530

    Time: 452 ms (53.34%) | Memory: 45.5 MB (16.73%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -1,24 +1,23 @@
 class Solution {
-    Map<Integer, Integer> map;
+    Set<Integer> set;
     public int track(int[] nums, int ind, int n, int k) {
         if(ind == n) {
             return 1;
         }
-        int ans = track(nums, ind + 1, n, k);
-        if (!map.containsKey(nums[ind] - k) && !map.containsKey(nums[ind] + k)) {
-            map.put(nums[ind], map.getOrDefault(nums[ind], 0) + 1);
-            ans += track(nums, ind + 1, n, k);
-            map.put(nums[ind], map.getOrDefault(nums[ind], 0) - 1);
-            if(map.get(nums[ind]) == 0){
-                map.remove(nums[ind]);
-            }
+        int pick = 0;
+        int dont = track(nums, ind + 1, n, k);
+        if(!set.contains(nums[ind] - k) && !set.contains(nums[ind] + k)){
+            set.add(nums[ind]);
+            pick += track(nums, ind + 1, n, k);
+            set.remove(nums[ind]);
         }
-        return ans;
+        return pick + dont;
     }
 
     public int beautifulSubsets(int[] nums, int k) {
-        map = new HashMap<>();
+        set = new HashSet<>();
         int n = nums.length;
+        Arrays.sort(nums);
         return track(nums, 0, n, k) - 1;
     }
 }
