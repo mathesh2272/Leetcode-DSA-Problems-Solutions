68973a08b5929e6797739c8a33fdb23146faeb0d
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-07-12 10:23:20 +0530

    Time: 187 ms (51.52%) | Memory: 49.5 MB (8.08%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,68 +1,31 @@
 class Solution {
+    int abba(Stack<Character> stack, char[] ch, int n, int x, char a, char b){
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(!stack.isEmpty() && stack.peek() == a && ch[i] == b){
+                stack.pop();
+                ans += x;
+            }
+            else{
+                stack.push(ch[i]);
+            }
+        }
+        return ans;
+    }
+    int remove(char[] ch, int n, int x, int y, char a, char b){
+        Stack<Character> stack = new Stack<>();
+        int ans = abba(stack, ch, n, x, a, b);
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        ch = sb.toString().toCharArray();
+        n = ch.length;
+        return ans + abba(stack, ch, n, y, a, b);
+    }
     public int maximumGain(String s, int x, int y) {
         char[] ch = s.toCharArray();
         int n = ch.length;
-        Stack<Character> stack = new Stack<>();
-        int ans = 0;
-        if(x > y){
-            for(int i = 0 ; i < n ; i++){
-                char c = ch[i];
-                if(!stack.isEmpty() && stack.peek() == 'a' && c == 'b'){
-                    stack.pop();
-                    ans += x;
-                }
-                else{
-                    stack.push(ch[i]);
-                }
-            }
-            StringBuilder sb = new StringBuilder();
-            while(!stack.isEmpty()){
-                sb.append(stack.pop());
-            }
-            sb.reverse();
-            ch = sb.toString().toCharArray();
-            n = ch.length;
-            for(int i = 0 ; i < n ; i++){
-                char c = ch[i];
-                if(!stack.isEmpty() && stack.peek() == 'b' && c == 'a'){
-                    stack.pop();
-                    ans += y;
-                }
-                else{
-                    stack.push(ch[i]);
-                }
-            }
-            return ans;
-        }
-        else{
-            for(int i = 0 ; i < n ; i++){
-                char c = ch[i];
-                if(!stack.isEmpty() && stack.peek() == 'b' && c == 'a'){
-                    stack.pop();
-                    ans += y;
-                }
-                else{
-                    stack.push(ch[i]);
-                }
-            }
-            StringBuilder sb = new StringBuilder();
-            while(!stack.isEmpty()){
-                sb.append(stack.pop());
-            }
-            sb.reverse();
-            ch = sb.toString().toCharArray();
-            n = ch.length;
-            for(int i = 0 ; i < n ; i++){
-                char c = ch[i];
-                if(!stack.isEmpty() && stack.peek() == 'a' && c == 'b'){
-                    stack.pop();
-                    ans += x;
-                }
-                else{
-                    stack.push(ch[i]);
-                }
-            }
-            return ans;
-        }
+        return x > y ? remove(ch, n, x, y, 'a', 'b') : remove(ch, n, y, x, 'b', 'a');
     }
 }
