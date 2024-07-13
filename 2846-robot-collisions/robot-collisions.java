2a10de7e8a4336ce0023b1d0effd0868ba160f3b
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-07-13 13:48:02 +0530

    Time: 107 ms (15.00%) | Memory: 64.9 MB (18.75%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -2,22 +2,20 @@ record Fri(int position, int health, char dir, int index){}
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
         int n = positions.length;
-        List<Fri> list = new ArrayList<>();
+        Fri[] fri = new Fri[n];
         char[] ch = directions.toCharArray();
         for(int i = 0 ; i < n ; i++){
-            list.add(new Fri(positions[i], healths[i], ch[i], i));
+            fri[i] = new Fri(positions[i], healths[i], ch[i], i);
         }
-        Collections.sort(list, (a, b) -> {
-            return a.position() - b.position();
-        });
+        Arrays.sort(fri, (a, b) -> a.position() - b.position());
         Stack<Fri> stack = new Stack<>();
         for(int i = 0 ; i < n ; i++){
-            Fri t = list.get(i);
-            if(!stack.isEmpty() && t != null && stack.peek().dir() == 'R' && t.dir() == 'L'){
+            Fri t = fri[i];
+            if(!stack.isEmpty() && stack.peek().dir() == 'R' && t.dir() == 'L'){
                 String f1 = t.toString();
                 String f2 = "";
                 boolean flag = true;
-                while(!stack.isEmpty() && t != null && stack.peek().dir() == 'R' && t.dir() == 'L'){
+                while(!stack.isEmpty() && stack.peek().dir() == 'R' && t.dir() == 'L'){
                     Fri tmp = stack.pop();
                     if(tmp.health() > t.health()){
                         stack.push(new Fri(tmp.position(), tmp.health() - 1, tmp.dir(), tmp.index()));
