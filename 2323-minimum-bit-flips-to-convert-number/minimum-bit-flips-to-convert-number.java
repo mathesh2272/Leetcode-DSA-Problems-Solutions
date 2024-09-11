c947d9f99d8b09c4600043783e669fb5b3910f97
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-09-11 09:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (47.90%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int ans = 0;
+        while(xor > 0){
+            ans++;
+            xor = xor & (xor - 1);
+        }
+        return ans;
+    }
+}
