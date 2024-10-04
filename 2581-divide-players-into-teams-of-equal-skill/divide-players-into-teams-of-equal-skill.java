d1646cd865ed61bcfab12e678a434a5abb02793b
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-10-04 13:35:45 +0530

    Time: 15 ms (76.57%) | Memory: 57.5 MB (63.48%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -3,8 +3,9 @@ class Solution {
         Arrays.sort(skills);
         int n = skills.length;
         long ans = 0;
+        int sum = skills[0] + skills[n - 1];
         for(int i = 0 ; i < n / 2 ; i++){
-            if(i + 1 < n && n - i - 2 >= 0 && skills[i] + skills[n - i - 1] != skills[i + 1] + skills[n - i - 2]){
+            if(skills[i] + skills[n - i - 1] != sum){
                 return -1;
             }
             ans = ans + (skills[i] * skills[n - i - 1]);
