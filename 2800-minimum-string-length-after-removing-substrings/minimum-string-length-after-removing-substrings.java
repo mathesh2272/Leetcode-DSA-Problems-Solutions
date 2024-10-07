08a7eb744b815f55c504bd389ff0716f768a9f6a
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2024-10-07 13:23:35 +0530

    Time: 27 ms (5.06%) | Memory: 45.7 MB (6.02%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minLength(String s) {
         while(s.contains("AB") || s.contains("CD")){
-            if(s.contains("AB")){
-                s = s.replaceAll("AB", "");
-            }
-            if(s.contains("CD")){
-                s = s.replaceAll("CD", "");
-            }
+            s = s.replaceAll("AB", "");
+            s = s.replaceAll("CD", "");
         }
         return s.length();
     }
