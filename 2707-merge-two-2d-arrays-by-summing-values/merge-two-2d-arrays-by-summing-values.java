4ca7d10d4764d3bb6160281248e011543f294e76
Mathesh M <137702099+mathesh2272@users.noreply.github.com>
2025-03-02 14:17:56 +0530

    Time: 2 ms (52.40%) | Memory: 45.2 MB (73.05%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        List<int[]> list = new ArrayList<>();
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < m){
+            if(nums1[i][0] < nums2[j][0]){
+                list.add(new int[]{nums1[i][0], nums1[i][1]});
+                i++;
+            }
+            else if(nums1[i][0] > nums2[j][0]){
+                list.add(new int[]{nums2[j][0], nums2[j][1]});
+                j++;
+            }
+            else{
+                list.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
+        }
+        while(i < n){
+            list.add(new int[]{nums1[i][0], nums1[i][1]});
+            i++;
+        }
+        while(j < m){
+            list.add(new int[]{nums2[j][0], nums2[j][1]});
+            j++;
+        }
+        return list.stream().toArray(size -> new int[size][]);
+    }
+}
